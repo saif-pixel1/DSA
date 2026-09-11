11c6f7beb7d1410812f6aa893f9bf136fa608b55
Saif Mansuri <rahulsingh.hashi1@gmail.com>
2026-09-11 21:37:22 +0530

    Time: 504 ms (84.9%), Space: 21.7 MB (99.67%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -3,7 +3,19 @@ class Solution:
         nums.sort()
         k = 0 
         res = []
+
+# [-1,0,1,2,-1,-4]
+# [-4,-1,-1,0,1,2]
+#           k
+#              l
+#                r
+#     
+#rfe = [[-1,-1,2],[-1,0,1]]
+# 
+
         while k<len(nums)-2:
+            if nums[k] > 0:
+                break
             if k > 0 and nums[k] == nums[k-1]:
                 k+=1
                 continue
